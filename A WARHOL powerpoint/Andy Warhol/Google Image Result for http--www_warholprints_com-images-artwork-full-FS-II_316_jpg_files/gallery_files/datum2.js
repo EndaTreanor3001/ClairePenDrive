713b1c8a0efd5f8c45f81c2a7543c318859f7911
EndaTreanor <enda_treanor@hotmail.com>
2019-02-18 19:46:27 +0000

    second commit

---
+++ A WARHOL powerpoint/Andy Warhol/Google Image Result for http--www_warholprints_com-images-artwork-full-FS-II_316_jpg_files/gallery_files/datum2.js
@@ -0,0 +1,20 @@
+<!--
+function writeDate() {
+var CurrentTime = "";
+CurrentYear = "";
+Todays = new Date();
+
+if (Todays.getYear() <=99)
+   {CurrentYear = "19"+ Todays.getYear();}
+else  if (Todays.getYear() <=199)
+   {CurrentYear = 1900 + Todays.getYear() ;}
+else
+   {CurrentYear = Todays.getYear() ;}
+TheDate = CurrentYear
+
+document.writeln(TheDate);
+}
+writeDate()
+
+// -->
+
